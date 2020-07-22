bb56529c039cb7e8521f236e6417b52a16731eb0
Brooks <68553910+Brooks6@users.noreply.github.com>
2020-07-23 05:01:58 +1000

    20200723_0501am

--- 20200723_notes.py
+++ 20200723_notes.py
@@ -25,6 +25,43 @@ my_num = 5.1
 print(floor(my_num)) #min, max, round, floor, ceil, sqrt and so on
 
 #getting input from users
+name = input("enter your name: ")#get a name from user
+print("hello "+ name + "!")
+
+#building a basic calculator
+num1 = input("enter a number")
+num2 = input("enter a another number:")
+result = int(num1) + int(num2)#ensure the number without decimal point
+print(result)
+
+#lists
+friends = ["Kevin", 2, false]
+print(friends)
+print(friends[-1])#the outcome is false, from right to left
+print(friends[1:])#output: 2, false
+print(friends[0:1])#output: Kevin, 2
+
+#lists functions
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tom"]
+friends.extend(lucky_numbers)#add 2 lists together
+friends.append("Creed")#add another element in the friends list
+friends.insert(1, "Kelly")#insert element in the the place where the index is 1 and other value move to the right, index++
+friends.remove("Kevin")
+friends.clear()
+friends.pop()#remove last element inside the list
+friends.sort()
+luck_numbers.reverse()#reverse the order of the list
+print(friends.index("Jim"))
+print(friends.count("Oscar"))#how many times the value Jim shows up inside the list
+friends2 = friends.copy()
+print(friends2)
+
+#tuples, a type of data structure
+coordinates = (4, 5)#list(),the tuples can not be changed
+print(coordinates[0])
+
+
 
 
 
