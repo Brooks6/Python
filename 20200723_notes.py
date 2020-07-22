c5602d2ba3765ce8e4700a9dd7bdd9064a80daba
Brooks <liuhongzhang1996@163.com>
2020-07-23 03:46:03 +1000

    20200723_0345am

---
+++ 20200723_notes.py
@@ -0,0 +1,37 @@
+#variables & data types
+character_name = "Brooks" #string
+character_age = 3.141 #number
+isMale = True #bool
+
+#working with string
+phrase = "Hello Brooks"
+print(phrase + " !")
+print(phrase.lower())#change all letters to lowercase
+print(phrase.isupper())#check if all the letters are uppercase
+print(len(phrase))#get the length of phrase
+print(phrase[0])#get the specified letter
+print(phrase.index("e"))#get index about specified letter
+print(phrase.replace("Hello", "Hi"))#replce certain letters
+
+#work with numbers
+num1 = 1
+print(str(num1) + "my favorite number")
+'''
+conver number into string
+number cant be combined with string directly
+'''
+from math import * #use math function
+my_num = 5.1
+print(floor(my_num)) #min, max, round, floor, ceil, sqrt and so on
+
+#getting input from users
+
+
+
+
+
+
+
+
+
+
